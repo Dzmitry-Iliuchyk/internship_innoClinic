417446ebbac795de43d2a0857ab25246be312bf2
Dmitry Iliuchyk <dima6061551@gmail.com>
2025-01-21 10:10:18 +0300

    gateway

--- innoClinic/ProfilesApi/Program.cs
+++ innoClinic/ProfilesApi/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<IIdentityService, IdentityService>();
 builder.Services.AddDataAccess(config);
 builder.Services.AddApplicationLayer();
+builder.Services.AddSingleton<ExceptionHandlingMiddleware>();
 builder.Services.AddControllers();
 builder.Services.AddLogging( opt => {
     opt.ClearProviders();
